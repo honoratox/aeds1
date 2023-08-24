48d9260e6986f06f64ba97c8d9ab77ee94302200
Pedro Honorato <111890943+honoratox@users.noreply.github.com>
2023-08-24 12:04:52 -0300

    Update atv01.c

--- ed01/atv01.c
+++ ed01/atv01.c
@@ -1,1 +1,28 @@
 
+/*
+  Author: Pedro Henrique de Magalhães Honorato
+  Matrícula: 784623
+  
+  - definir e ler um valor inteiro do teclado;
+  - supor que esse valor represente o lado de um quadrado, calcular e mostrar a área de outro quadrado com o lado seis vezes maior.
+  DICA: Usar constantes reais em expressões que envolvam valores reais.
+*/
+
+#include <stdio.h>
+
+int main() {
+
+  int n;
+  int lado6;
+  int area;
+
+  printf("\n Digite um valor do teclado: ");
+  scanf("%d", &n);
+
+  lado6 = n*6;
+  area = lado6*lado6;
+  
+  printf("\n Quadrado 6x maior tem area de : %d", area);
+
+  return 0;
+}
